634dc6d5bc2f25b9bf652217913bd76e18a846f1
Saqibkhan-engineer <msaqikhan005@gmail.com>
2024-11-18 10:58:26 +0500

    Update TransportObserverImpl.java

--- LAB mid term/JavaApplication2/src/TransportObserverImpl.java
+++ LAB mid term/JavaApplication2/src/TransportObserverImpl.java
@@ -5,7 +5,7 @@ interface TransportObserver {
     void update(TransportUseCase useCase);
 }
 
-// Concrete Observer
+
 class TransportObserverImpl implements TransportObserver {
     @Override
     public void update(TransportUseCase useCase) {
@@ -13,21 +13,20 @@ class TransportObserverImpl implements TransportObserver {
     }
 }
 
-// Subject that manages observers and notifies them
+
 class TransportSubject {
     private List<TransportObserver> observers = new ArrayList<>();
 
-    // Add an observer
     public void addObserver(TransportObserver observer) {
         observers.add(observer);
     }
 
-    // Remove an observer
+
     public void removeObserver(TransportObserver observer) {
         observers.remove(observer);
     }
 
-    // Notify all observers
+   
     public void notifyObservers(TransportUseCase useCase) {
         for (TransportObserver observer : observers) {
             observer.update(useCase);
