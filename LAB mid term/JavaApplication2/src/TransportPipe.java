a6a3b6bc6be470b022904560797f75373cc43edc
Saqibkhan-engineer <msaqikhan005@gmail.com>
2024-11-18 10:58:56 +0500

    Update TransportPipe.java

--- LAB mid term/JavaApplication2/src/TransportPipe.java
+++ LAB mid term/JavaApplication2/src/TransportPipe.java
@@ -8,18 +8,17 @@
  *
  * @author FA22-BSE-005
  */
-// Pipe to apply a sequence of filters on the transport use case
+
 import java.util.List;
 import java.util.ArrayList;
 class TransportPipe {
     private List<TransportFilter> filters = new ArrayList<>();
 
-    // Add filter to the pipe
+   
     public void addFilter(TransportFilter filter) {
         filters.add(filter);
     }
 
-    // Apply filters to a use case
     public void applyFilters(TransportUseCase useCase) {
         for (TransportFilter filter : filters) {
             filter.apply(useCase);
