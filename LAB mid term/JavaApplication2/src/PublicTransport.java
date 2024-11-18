6dc73c8e9f2413ffb322ed9df1b6b20f31a90300
Saqibkhan-engineer <msaqikhan005@gmail.com>
2024-11-18 10:57:59 +0500

    Update PublicTransport.java

--- LAB mid term/JavaApplication2/src/PublicTransport.java
+++ LAB mid term/JavaApplication2/src/PublicTransport.java
@@ -8,21 +8,21 @@
  *
  * @author FA22-BSE-005
  */
-// PublicTransport use case class
+
 class PublicTransport extends TransportUseCase {
     public PublicTransport() {
         super("Public Transport", "Pay on spot", "Flexible", "No restrictions");
     }
 }
 
-// OwnConvince use case class
+
 class OwnConvince extends TransportUseCase {
     public OwnConvince() {
         super("Own Convince", "Pay as you use", "Flexible schedule", "No restrictions");
     }
 }
 
-// UniversityTransport use case class
+
 class UniversityTransport extends TransportUseCase {
     public UniversityTransport() {
         super("University Transport", "Advance payment", "Time restricted", "Fixed schedule");
